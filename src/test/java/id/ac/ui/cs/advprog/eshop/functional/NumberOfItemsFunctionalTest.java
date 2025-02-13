12984ba6204e4bdc72f9dcb9e14c242f9ecac942
Paima <paimapurba@gmail.com>
2025-02-13 11:48:06 +0700

    create numberOfItemsFunctionalTest

--- src/test/java/id/ac/ui/cs/advprog/eshop/functional/NumberOfItemsFunctionalTest.java
+++ src/test/java/id/ac/ui/cs/advprog/eshop/functional/NumberOfItemsFunctionalTest.java
@@ -11,12 +11,14 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.server.LocalServerPort;
 
+import java.util.List;
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.springframework.boot.test.context.SpringBootTest.WebEnvironment.RANDOM_PORT;
 
 @SpringBootTest(webEnvironment = RANDOM_PORT)
 @ExtendWith(SeleniumJupiter.class)
-class CreateProductFunctionalTest {
+class NumberOfItemsFunctionalTest {
     @LocalServerPort
     private int serverPort;
 
@@ -31,14 +33,11 @@ class CreateProductFunctionalTest {
     }
 
     @Test
-    void addProductButtonClick_isCorrect(ChromeDriver driver) {
+    void numberOfItemsIs1_isCorrect(ChromeDriver driver) {
         driver.get(baseUrl);
         WebElement createButton = driver.findElement(By.id("create-button"));
 
         createButton.click();
-
-        String expectedUrl = String.format("http://localhost:%d/product/create", serverPort);
-        assertEquals(expectedUrl, driver.getCurrentUrl());
 
         WebElement productNameField = driver.findElement(By.id("nameInput"));
         WebElement productQuantityField = driver.findElement(By.id("quantityInput"));
@@ -51,12 +50,34 @@ class CreateProductFunctionalTest {
         WebElement submitButton = driver.findElement(By.id("submit-button"));
         submitButton.click();
 
-        expectedUrl = String.format("http://localhost:%d/product/list", serverPort);
-        assertEquals(expectedUrl, driver.getCurrentUrl());
+        List<WebElement> productRows = driver.findElements(By.xpath("//tbody/tr"));
 
-        WebElement productName = driver.findElement(By.id("product-name"));
-        WebElement productQuantity = driver.findElement(By.id("product-quantity"));
-        assertEquals("Test", productName.getText());
-        assertEquals("10", productQuantity.getText());
+        assertEquals(1, productRows.size());
+    }
+
+    @Test
+    void numberOfItemsIs3_isCorrect(ChromeDriver driver) {
+        for (int i = 0; i < 3; i++) {
+            driver.get(baseUrl);
+            WebElement createButton = driver.findElement(By.id("create-button"));
+
+            createButton.click();
+
+            WebElement productNameField = driver.findElement(By.id("nameInput"));
+            WebElement productQuantityField = driver.findElement(By.id("quantityInput"));
+            productNameField.clear();
+            productQuantityField.clear();
+
+            productNameField.sendKeys("Test");
+            productQuantityField.sendKeys("10");
+
+            WebElement submitButton = driver.findElement(By.id("submit-button"));
+            submitButton.click();
+        }
+
+
+        List<WebElement> productRows = driver.findElements(By.xpath("//tbody/tr"));
+
+        assertEquals(3, productRows.size());
     }
 }
