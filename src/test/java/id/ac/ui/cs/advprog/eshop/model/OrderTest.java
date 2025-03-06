7a4cf4efe3175c87272aea1a7c95be8580327697
Paima <paimapurba@gmail.com>
2025-03-06 21:54:44 +0700

    [GREEN] Implement Order model

--- src/test/java/id/ac/ui/cs/advprog/eshop/model/OrderTest.java
+++ src/test/java/id/ac/ui/cs/advprog/eshop/model/OrderTest.java
@@ -50,7 +50,7 @@ class OrderTest {
         assertSame(this.products, order.getProducts());
         assertEquals(2, order.getProducts().size());
         assertEquals("Sampo Cap Bambang", order.getProducts().get(0).getProductName());
-        assertEquals("Sabun Cap Usep", order.getProducts().get(1).getProductName());
+        assertEquals("Sampo Cap Usep", order.getProducts().get(1).getProductName());
 
         assertEquals("13652556-012a-4c07-b546-54eb1396d79b", order.getId());
         assertEquals(1708560000L, order.getOrderTime());
@@ -109,4 +109,5 @@ class OrderTest {
 
         assertThrows(IllegalArgumentException.class, () -> order.setStatus("MEOW"));
     }
+
 }
