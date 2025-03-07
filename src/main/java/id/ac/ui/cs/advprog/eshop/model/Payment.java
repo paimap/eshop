30c077329097216ecc97516d1ec442465af94b22
Paima <paimapurba@gmail.com>
2025-03-07 23:13:59 +0700

    [GREEN] Implement Payment model

--- src/main/java/id/ac/ui/cs/advprog/eshop/model/Payment.java
+++ src/main/java/id/ac/ui/cs/advprog/eshop/model/Payment.java
@@ -3,6 +3,7 @@ package id.ac.ui.cs.advprog.eshop.model;
 import lombok.Builder;
 import lombok.Getter;
 
+import java.util.Arrays;
 import java.util.Map;
 
 @Builder
@@ -14,9 +15,28 @@ class Payment {
     Map<String, String> paymentData;
 
     Payment(String id, String method, String status) {
+        this.id = id;
+
+        String [] statusArray = {"REJECTED", "SUCCESS"};
+        if(Arrays.stream(statusArray).noneMatch(x -> x.equals(status))) {
+            throw new IllegalArgumentException();
+        } else {
+            this.status = status;
+        }
+
+        String [] methodArray = {"bankTransfer", "voucherCode"};
+        if(Arrays.stream(methodArray).noneMatch(x -> x.equals(method))) {
+            throw new IllegalArgumentException();
+        }else {
+            this.method = method;
+        }
     }
 
     Payment(String id, String method, String status, Map<String, String> paymentData) {
+        this.id = id;
+        this.method = method;
+        this.status = status;
+        this.paymentData = paymentData;
     }
 
 }
